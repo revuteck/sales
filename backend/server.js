6e388d9ed8527618b299d334923cb4246341b51e
harshavardhan1905 <podetiharshavardhan@gmail.com>
2026-01-02 19:51:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 // ---- ENABLE CORS BEFORE ROUTES ----
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://moccasin-viper-357897.hostingersite.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
