f458a105fb40cd625672d6f683c9e79dfcf4a0b2
revuteck <revuteckutej@gmail.com>
2026-01-05 11:05:38 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const db = mysql.createPool({
-  host: ls-b9e1fff8ae66188406492f1b8709a5557d98a493.cvq4mo8ooiu8.ap-south-1.rds.amazonaws.com,
-  user: dbmasteruser,
-  password: Sales1221.,
-  database: sales,
+  host: "ls-b9e1fff8ae66188406492f1b8709a5557d98a493.cvq4mo8ooiu8.ap-south-1.rds.amazonaws.com",
+  user: "dbmasteruser",
+  password: "Sales1221.",
+  database: "sales"",
   port: 3306
 });
 
