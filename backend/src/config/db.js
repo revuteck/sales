0963ca02bd7ac77d8268a6516ce96893942bb960
revuteck <revuteckutej@gmail.com>
2026-01-05 11:07:42 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,8 +5,8 @@ const db = mysql.createPool({
   host: "ls-b9e1fff8ae66188406492f1b8709a5557d98a493.cvq4mo8ooiu8.ap-south-1.rds.amazonaws.com",
   user: "dbmasteruser",
   password: "Sales1221.",
-  database: "sales"",
-  port: 3306
+  database: "sales",
+  port: 3306,
 });
 
 db.getConnection((err, connection) => {
