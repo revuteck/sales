2156d08242299baef56a30eb0ed9b9a56059b510
revuteck <revuteckutej@gmail.com>
2026-01-05 13:11:29 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,10 +4,10 @@ require("dotenv").config({ path: require("path").join(__dirname, "../.env") });
 
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: "ls-b9e1fff8ae66188406492f1b8709a5557d98a493.cvq4mo8ooiu8.ap-south-1.rds.amazonaws.com",
+  user: "dbmasteruser",
+  password: "Sales1221.",
+  database: "sales",
   port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
