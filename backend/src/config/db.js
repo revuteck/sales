828444aaabd7cbfc7f74c23f2ddb905a85c10bef
revuteck <revuteckutej@gmail.com>
2026-01-05 10:12:26 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOS,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
