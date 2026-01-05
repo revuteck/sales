8611172491e419b7f5c602023b58ee9d9666fd98
revuteck <revuteckutej@gmail.com>
2026-01-05 11:22:34 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,25 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
+// const db = mysql.createPool({
+//   host: "localhost",
+//   user: "u122144590_clientsales",
+//   password: "Revuteck@2101.",
+//   database: "u122144590_clientsales",
+//   port: 3306,
+// });
+
 const db = mysql.createPool({
-  host: "127.0.0.1",
+  host: "localhost",
   user: "u122144590_clientsales",
   password: "Revuteck@2101.",
   database: "u122144590_clientsales",
   port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
+
 
 db.getConnection((err, connection) => {
   if (err) {
